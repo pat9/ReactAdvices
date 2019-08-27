66718b0553376da12e8b4fbb5869c123984fd9ce
Daniel Pat <danielpat6@gmail.com>
2019-08-26 21:47:19 -0500

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  const [advice, setAdvice] = useState('');
+
+  useEffect(()=>{
+    async function getAdvice(){
+      if(advice !== "") return;
+      const respuesta =  await fetch('https://api.adviceslip.com/advice')
+      const data = await respuesta.json();
+  
+  
+      setAdvice(data.slip.advice)
+      
+      
+    }
+    getAdvice();
+  }, [advice])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="React logo"/>
+        <h3>React Advices</h3>
+      </div>
+      <div className="App">
+        <div className="adviceContainer">
+          <p className="advice">{advice}</p>
+        </div>
+      </div>
+      <footer>
+        <p>Daniel Pat &copy; / Adivices by <a href="https://api.adviceslip.com/">Advice Slip JSON API</a></p>
+      </footer>
+    </>
   );
 }
 
